9e3ee601934e2b40f77ce0676e6e12eafa585388
SK-x7 <111245838+SK-x7@users.noreply.github.com>
2024-11-20 02:27:57 +0530

    created some components and pages for the layout

---
+++ app/_components/Counter.js
@@ -0,0 +1,15 @@
+"use client"
+import { useState } from "react"
+
+function Counter() {
+    const [count,setCount]=useState(0);
+    
+    
+    return (
+        <div>
+            {count}
+        </div>
+    )
+}
+
+export default Counter
