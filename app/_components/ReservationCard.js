822591310891368e578bd1fce05aa0652601fe8e
SK-x7 <111245838+SK-x7@users.noreply.github.com>
2024-11-28 20:41:52 +0530

    completed 'Delete reservation functionality'.

--- app/_components/ReservationCard.js
+++ app/_components/ReservationCard.js
@@ -1,5 +1,6 @@
 import { PencilSquareIcon } from '@heroicons/react/24/solid';
 import { format, formatDistance, isPast, isToday, parseISO } from 'date-fns';
+import Link from 'next/link';
 import DeleteReservation from './DeleteReservation';
 
 export const formatDistanceFromNow = (dateStr) =>
@@ -68,14 +69,19 @@ function ReservationCard({ booking }) {
       </div>
 
       <div className='flex flex-col border-l border-primary-800 w-[100px]'>
-        <a
-          href={`/account/reservations/edit/${id}`}
-          className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'
+        {
+          !isPast(startDate)&&
+          <>
+        <Link
+        href={`/account/reservations/edit/${id}`}
+        className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'
         >
           <PencilSquareIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
           <span className='mt-1'>Edit</span>
-        </a>
+        </Link>
         <DeleteReservation bookingId={id} />
+        </>
+      }
       </div>
     </div>
   );
